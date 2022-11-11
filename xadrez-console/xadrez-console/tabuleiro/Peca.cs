cf65d3ec871a6bdb94c67c2311eb3b273b45de56
Lucas-Cason <89609335+Lucas-Cason@users.noreply.github.com>
2022-11-11 08:56:37 -0300

    Add files via upload

---
+++ xadrez-console/xadrez-console/tabuleiro/Peca.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tabuleiro
+{
+     abstract class Peca
+    {
+        public Posicao posicao { get; set; }
+        public Cor cor { get; protected set; }
+        public int qtdMovimentos { get; protected set; }
+        public Tabuleiro tab { get; protected set; }
+
+        public Peca(Tabuleiro tab, Cor cor)
+        {
+            this.posicao = null;
+            this.cor = cor;
+            this.tab = tab;
+            this.qtdMovimentos = 0;
+        }
+
+        public abstract bool[,] movimentosPossiveis();
+
+        public void incrementarQtdMovimentos()
+        {
+            qtdMovimentos++;
+        }
+
+        public void decrementarQtdMovimentos()
+        {
+            qtdMovimentos--;
+        }
+
+        public bool existeMovimentosPossiveis()
+        {
+            bool[,] mat = movimentosPossiveis();
+            for(int i = 0; i < tab.linhas; i++)
+            {
+                for(int j = 0; j < tab.colunas; j++)
+                {
+                    if (mat[i, j])
+                    {
+                        return true;
+                    }
+                }
+            }
+            return false;
+        }
+
+        public bool movimentoPossivel(Posicao pos)
+        {
+            return movimentosPossiveis()[pos.linha, pos.coluna];
+        }
+    }
+}
